17c9b89c498dc0fabcb64c37cb9d9d55ed64bca1
Oskar Schöldström <m@oxy.fi>
2017-06-13 08:27:48 -0500

    fix large image size set to 0

--- app/images.php
+++ app/images.php
@@ -6,8 +6,9 @@ namespace App;
  * Set the maximum allowed width for any content (eg. oEmbeds, images). This
  * should be the width of the content area.
  */
+global $content_width;
 if (!isset($content_width)) {
-    $content_width = 1000;
+    $content_width = 800;
 }
 
 /**
@@ -17,7 +18,7 @@ add_action('after_setup_theme', function () {
     global $content_width;
 
     // Modify Core sizes.
-    if (get_option('large_size_h') != $content_width/1.5) {
+    if (get_option('large_size_w') != $content_width) {
         update_option('thumbnail_size_w', 150);
         update_option('thumbnail_size_h', 150);
         update_option('thumbnail_crop', 1);
@@ -25,7 +26,7 @@ add_action('after_setup_theme', function () {
         update_option('medium_size_h', 300);
         // Desktop Version
         update_option('large_size_w', $content_width);
-        update_option('large_size_h', $content_width/1.5);
+        update_option('large_size_h', round($content_width/1.5));
     }
 
     // Tablet version
