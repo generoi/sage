0ec75e0d570b3f276e3971a3bd0e099d8db40167
Oskar Schöldström <m@oxy.fi>
2017-07-07 11:58:01 -0500

    remove lqip technique from lazyloaded images

--- app/images.php
+++ app/images.php
@@ -36,9 +36,6 @@ add_action('after_setup_theme', function () {
     add_image_size('hero--tablet', 768, 400, 1);
     add_image_size('hero--mobile', 400, 350, 1);
 
-    // Lazyloaded preview
-    add_image_size('tiny-lazy', 30, 30);
-
     // Tablet version
     add_image_size('tablet', 768, 400);
     // Mobile version
@@ -105,37 +102,15 @@ function filter_lazyload_images($content) {
         if (preg_match("/src=['\"]data:image/is", $img_tag)) {
             continue;
         }
-        $placeholder_url_used = $placeholder_url;
-        // If there's a reference to the attachment id, use a datauri embedded thumbnail.
-        if (preg_match('/class=["\'].*?wp-image-([0-9]*)/is', $img_tag, $id_matches)) {
-            $img_id = intval($id_matches[1]);
-            $tiny_img_data  = wp_get_attachment_image_src($img_id, 'tiny-lazy');
-            $tiny_url = $tiny_img_data[0];
-
-            $cid = 'datauri_' . substr(md5($tiny_url), 0, 6);
-            if (!($datauri = get_transient($cid))) {
-                $tiny_path = Timber\ImageHelper::get_server_location($tiny_url);
-                if (file_exists($tiny_path)) {
-                    $base64 = base64_encode(file_get_contents($tiny_path));
-                    $mime = mime_content_type($tiny_path);
-                    $datauri = "data:$mime;base64,$base64";
-                    set_transient($cid, $datauri);
-                }
-            }
-            if ($datauri) {
-                $placeholder_url_used = $datauri;
-            }
-        }
-
         // Replace the src and add the data-src attribute
-        $lazy_tag = preg_replace('/<img(.*?)src=/is', '<img$1src="' . esc_attr($placeholder_url_used) . '" data-src=', $img_tag);
+        $lazy_tag = preg_replace('/<img(.*?)src=/is', '<img$1src="' . esc_attr($placeholder_url) . '" data-src=', $img_tag);
         // Replace the srcset
         $lazy_tag = str_replace('srcset', 'data-srcset', $lazy_tag);
         // Add the lazy class to the img element
         if (preg_match('/class=["\']/i', $lazy_tag)) {
-            $lazy_tag = preg_replace('/class=(["\'])(.*?)["\']/is', 'class=$1lazyload blur $2$1', $lazy_tag);
+            $lazy_tag = preg_replace('/class=(["\'])(.*?)["\']/is', 'class=$1lazyload $2$1', $lazy_tag);
         } else {
-            $lazy_tag = preg_replace('/<img/is', '<img class="lazyload blur"', $lazy_tag);
+            $lazy_tag = preg_replace('/<img/is', '<img class="lazyload"', $lazy_tag);
         }
         $lazy_tag .= sprintf('<noscript>%s</noscript>', $img_tag);
         $search[] = $img_tag;
