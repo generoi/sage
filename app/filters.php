99ff1d46123739a56da12f02ecc46c7946f9f104
Oskar Schöldström <m@oxy.fi>
2017-07-07 11:53:29 -0500

    add support for gallery shortcode

--- app/filters.php
+++ app/filters.php
@@ -105,3 +105,91 @@ add_filter('embed_oembed_html', function ($cache, $url, $attr, $post_id) {
     }
     return '<div class="responsive-embed widescreen">' . $cache . '</div>';
 }, 10, 4);
+
+/**
+ * WP Gallery and Foundation integration.
+ * @see https://developer.wordpress.org/reference/functions/gallery_shortcode/
+ */
+add_filter('post_gallery', function ($output, $attr, $instance) {
+    $post = get_post();
+    $atts = shortcode_atts([
+        'order'      => 'ASC',
+        'orderby'    => 'menu_order ID',
+        'id'         => $post ? $post->ID : 0,
+        'itemtag'    => 'figure',
+        'icontag'    => 'div',
+        'captiontag' => 'figcaption',
+        'columns'    => 3,
+        'size'       => 'thumbnail',
+        'include'    => '',
+        'exclude'    => '',
+        'link'       => ''
+    ], $attr, 'gallery');
+
+    $id = intval($atts['id']);
+
+    if (!empty($atts['include'])) {
+        $_attachments = get_posts(['include' => $atts['include'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby']]);
+        $attachments = [];
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif (!empty($atts['exclude'])) {
+        $attachments = get_children(['post_parent' => $id, 'exclude' => $atts['exclude'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby']]);
+    } else {
+        $attachments = get_children(['post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby']]);
+    }
+
+    if (empty($attachments)) {
+        return '';
+    }
+
+    if (is_feed()) {
+        $output = "\n";
+        foreach ($attachments as $att_id => $attachment) {
+            $output .= wp_get_attachment_link($att_id, $atts['size'], true) . "\n";
+        }
+        return $output;
+    }
+
+    $itemtag = tag_escape($atts['itemtag']);
+    $captiontag = tag_escape($atts['captiontag']);
+    $icontag = tag_escape($atts['icontag'] );
+    $valid_tags = wp_kses_allowed_html('post');
+
+    $columns = intval($atts['columns']);
+    $itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+    $float = is_rtl() ? 'right' : 'left';
+
+    $selector = "gallery-{$instance}";
+
+    $size_class = sanitize_html_class($atts['size']);
+    $grid_class = "grid-x grid-margin-x grid-margin-y small-up-1 medium-up-{$columns} align-center align-middle";
+    $gallery_div = "<div id='$selector' class='gallery galleryid-{$id} {$grid_class}'>";
+    $output = apply_filters('gallery_style', $gallery_div);
+
+    $i = 0;
+    foreach ($attachments as $id => $attachment) {
+        $attr = (trim($attachment->post_excerpt)) ? ['aria-describedby' => "$selector-$id"] : '';
+        if (!empty($atts['link']) && 'file' === $atts['link']) {
+            $image_output = wp_get_attachment_link($id, $atts['size'], false, false, false, $attr);
+        } elseif (!empty($atts['link']) && 'none' === $atts['link'] ) {
+            $image_output = wp_get_attachment_image($id, $atts['size'], false, $attr);
+        } else {
+            $image_output = wp_get_attachment_link($id, $atts['size'], true, false, false, $attr);
+        }
+        $image_meta  = wp_get_attachment_metadata($id);
+        $orientation = '';
+        if (isset($image_meta['height'], $image_meta['width'])) {
+            $orientation = ($image_meta['height'] > $image_meta['width']) ? 'portrait' : 'landscape';
+        }
+        $output .= "<{$itemtag} class='gallery-item cell'>";
+        $output .= "<{$icontag} class='gallery-icon {$orientation}'>$image_output</{$icontag}>";
+        if ($captiontag && trim($attachment->post_excerpt)) {
+            $output .= "<{$captiontag} class='wp-caption-text gallery-caption' id='$selector-$id'>" . wptexturize($attachment->post_excerpt) . "</{$captiontag}>";
+        }
+        $output .= "</{$itemtag}>";
+    }
+    $output .= "</div>\n";
+    return $output;
+}, 10, 3);
