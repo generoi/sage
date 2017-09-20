cb4c3e4226b975ab77ae167d0309f741e39f8e80
Oskar Schöldström <m@oxy.fi>
2017-09-20 16:14:39 -0500

    fix yoast php notice

--- app/yoast.php
+++ app/yoast.php
@@ -15,10 +15,12 @@ namespace App;
 add_filter('wpseo_replacements', function ($replacements) {
     if (is_tax() || is_category() || is_tag()) {
         $term = $GLOBALS['wp_query']->get_queried_object();
-        $taxonomy = $term->taxonomy;
-        if ($term->parent != 0) {
-            $parent = get_term($term->parent, $taxonomy);
-            $replacements['%%term_parent_title%%'] = $parent->name;
+        if (isset($term->taxonomy)) {
+            $taxonomy = $term->taxonomy;
+            if ($term->parent != 0) {
+                $parent = get_term($term->parent, $taxonomy);
+                $replacements['%%term_parent_title%%'] = $parent->name;
+            }
         }
     }
     return $replacements;
