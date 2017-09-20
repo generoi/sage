04b0cfc19c48149d2b6c71c50d93772bd370828c
Oskar Schöldström <m@oxy.fi>
2017-09-20 16:14:08 -0500

    fix polylang acf php notice

--- app/acf.php
+++ app/acf.php
@@ -33,3 +33,13 @@ add_filter('acf/load_field/name=slide_overlay', function ($field) {
 add_filter('acf/settings/google_api_key', function ($value) {
     return '';
 });
+
+/**
+ * Fix Polylang acf.php:62 undefined post_id PHP notice.
+ */
+add_filter('acf/location/screen', function ($screen, $field_group) {
+    if (function_exists('PLL') && !isset($screen['post_id'])) {
+        $screen['post_id'] = null;
+    }
+    return $screen;
+}, 10, 2);
