2c8a3c9d09c9ac5255f4ba5b477aa9c2097b1bba
Oskar Schöldström <m@oxy.fi>
2018-06-27 12:34:54 -0300

    add vendor prefixes for grid

--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -4,11 +4,15 @@ const cssnanoConfig = {
   preset: ['default', { discardComments: { removeAll: true } }]
 };
 
+const autoprefixerConfig = {
+  grid: true
+}
+
 module.exports = ({ file, options }) => {
   return {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
-      autoprefixer: true,
+      autoprefixer: autoprefixerConfig,
       cssnano: options.enabled.optimize ? cssnanoConfig : false,
     },
   };
