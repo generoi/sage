f0bd53a49ace15e02e54d925a8f18deb865ef603
Oskar Schöldström <m@oxy.fi>
2019-06-05 08:45:48 -0300

    set custom oembed url parameters for youtube/vimeo

--- app/filters.php
+++ app/filters.php
@@ -17,3 +17,55 @@ namespace App;
 add_filter('excerpt_more', function () {
     return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 });
+
+/**
+ * Modify oEmbed URL parameters.
+ */
+add_filter('embed_oembed_html', function ($cache, $url, $attr, $post_id) {
+    preg_match('/src="([^"]*)"/i', $cache, $sources);
+    if (!empty($sources)) {
+        $src = $sources[1];
+    }
+    if (!empty($src) && !empty($url)) {
+        $is_youtube = strpos($src, 'youtube') !== false;
+        $is_vimeo = strpos($src, 'vimeo') !== false;
+        if ($is_youtube) {
+            // @see https://developers.google.com/youtube/player_parameters#Parameters
+            $args = [
+                'rel' => 0,
+                'modestbranding' => 1,
+            ];
+        } elseif ($is_vimeo) {
+            $args = [
+                'title' => 0,
+                'byline' => 0,
+                'portrait' => 0,
+                'controls' => 0,
+                'iv_load_policy' => 3,
+            ];
+        }
+        if (!empty($args) && ($parts = parse_url($src))) {
+            $query = !empty($parts['query']) ? wp_parse_args($parts['query']) : [];
+            // Override URL attributes with shortcode ones.
+            $query = array_merge($query, $attr);
+            // Add in defaults unless they are already defined.
+            $query = array_merge($args, $query);
+            // Force /embed endpoint for youtube.
+            if ($is_youtube && $parts['path'] == '/watch') {
+                $parts['path'] = '/embed/' . $query['v'];
+                unset($query['v']);
+            }
+            if ($is_vimeo && is_numeric(substr($parts['path'], 1))) {
+                $parts['host'] = 'player.vimeo.com';
+                $parts['path'] = "/video{$parts['path']}";
+            }
+            // Use schemeless URL and re-build the query.
+            $parts['scheme'] = null;
+            $parts['query'] = build_query($query);
+            // Rebuild the URL
+            $url = build_url($parts);
+            $cache = str_replace($src, $url, $cache);
+        }
+    }
+    return $cache;
+}, 10, 4);
--- app/helpers.php
+++ app/helpers.php
@@ -8,3 +8,24 @@
  */
 
 namespace App;
+
+/**
+ * Build a URL string based on URL parts.
+ * @see https://stackoverflow.com/a/35207936/319855
+ *
+ * @param array $parts Parts of the URL as returned by `parse_url`
+ * @return string
+ */
+function build_url($parts)
+{
+    return (isset($parts['scheme']) ? "{$parts['scheme']}:" : '') .
+        ((isset($parts['user']) || isset($parts['host'])) ? '//' : '') .
+        (isset($parts['user']) ? "{$parts['user']}" : '') .
+        (isset($parts['pass']) ? ":{$parts['pass']}" : '') .
+        (isset($parts['user']) ? '@' : '') .
+        (isset($parts['host']) ? "{$parts['host']}" : '') .
+        (isset($parts['port']) ? ":{$parts['port']}" : '') .
+        (isset($parts['path']) ? "{$parts['path']}" : '') .
+        (isset($parts['query']) ? "?{$parts['query']}" : '') .
+        (isset($parts['fragment']) ? "#{$parts['fragment']}" : '');
+}
