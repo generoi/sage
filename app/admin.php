b5a0fe79c24d82c27c03cb70353b5ac5fa96aa77
Oskar Schöldström <m@oxy.fi>
2019-06-05 08:34:07 -0300

    add styles to tinymce editors

--- app/admin.php
+++ app/admin.php
@@ -30,6 +30,42 @@ add_action('customize_register', function (WP_Customize_Manager $wp_customize) {
 });
 
 /**
+ * Add styling classes to TinyMCE.
+ */
+add_filter('tiny_mce_before_init', function ($settings) {
+    $style_formats = [
+        [
+            'title' => 'Buttons',
+            'items' => [
+                ['title' => 'Buttons', 'selector' => 'a', 'classes' => 'button'],
+            ],
+        ],
+    ];
+    $settings['style_formats'] = json_encode($style_formats);
+    $settings['style_formats_merge'] = true;
+
+    // Fix constantly growing editor.
+    if (!isset($settings['content_style'])) {
+        $settings['content_style'] = '';
+    }
+    $settings['content_style'] = 'html { height: auto !important; min-height: initial !important; max-height: initial !important; }';
+
+    return $settings;
+});
+
+/**
+ * Modify buttons in TinyMCE's second row.
+ */
+add_filter('mce_buttons_2', function ($buttons) {
+    // Unless TinyMCE Advanced is enabled, we need to specifically add the style button.
+    array_splice($buttons, 1, 0, 'styleselect');
+    $remove = [
+        'forecolor', // text color
+    ];
+    return array_diff($buttons, $remove);
+});
+
+/**
  * Register the customizer assets.
  *
  * @return void
